5f5a1fc2a6ea8285aa7bc68a3865c901ac0f2648
Ilya Sayenko <ilyasin060495@gmail.com>
2022-09-29 15:37:23 +0300

    cs code added

---
+++ program/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] GetWordsLenThree(string[] array) {
+    int newArrLen = 0;
+    foreach (string s in array) {
+        if (s.Length <= 3) {
+            newArrLen++;
+        }
+    }
+
+    string[] newArr = new string[newArrLen];
+
+    if (newArrLen > 0) {
+        int index = 0;
+
+        foreach (string s in array) {
+            if (s.Length <= 3) {
+                newArr[index] = s;
+                index++;
+            }
+        }
+    }  
+
+    return newArr;
+}
+
+System.Console.WriteLine(String.Join(", ", GetWordsLenThree(new string[]{"Hello", "2", "word", ":-)"})));
+System.Console.WriteLine(String.Join(", ", GetWordsLenThree(new string[]{"Russia", "Denmark", "Kazan"})));
+System.Console.WriteLine(String.Join(", ", GetWordsLenThree(new string[]{"1234", "1567", "-2", "computer science"})));
